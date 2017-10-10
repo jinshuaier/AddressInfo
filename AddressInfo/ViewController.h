23afe0333ab558b502404786a8f53d2ce19e3773
jinshuaier <470401910@qq.com>
2017-10-10 10:43:20 +0800

    first commit

--- AddressInfo/ViewController.h
+++ AddressInfo/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void(^ReturnCityName)(NSString *cityname);
+
 @interface ViewController : UIViewController
 
+@property (nonatomic, copy) ReturnCityName returnBlock;
 
+- (void)returnText:(ReturnCityName)block;
 @end
 
